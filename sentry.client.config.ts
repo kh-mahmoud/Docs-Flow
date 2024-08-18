801e45984ce9ec2ef0e4b859a66949df7fbbf416
Khouchane Mahmoud <97807779+kh-mahmoud@users.noreply.github.com>
2024-08-18 23:08:14 +0100

    new-update

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -25,11 +25,5 @@ Sentry.init({
       maskAllText: true,
       blockAllMedia: true,
     }),
-    Sentry.feedbackIntegration({
-      // Additional SDK configuration goes in here, for example:
-      colorScheme: "system",
-      isNameRequired: true,
-      isEmailRequired: true,
-    }),
   ],
 });
