0a84adc8723be65e276344a7bf8392cceea26744
Khouchane Mahmoud <97807779+kh-mahmoud@users.noreply.github.com>
2024-08-19 00:06:18 +0100

    new-nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,20 @@
-import {withSentryConfig} from '@sentry/nextjs';
+import { withSentryConfig } from '@sentry/nextjs';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+    typescript: {
+        ignoreBuildErrors: true,
+    },
     images: {
         remotePatterns: [
             {
                 protocol: 'https',
                 hostname: 'img.clerk.com',
-            }
+                port: '',
+                pathname: '/**',
+            },
         ],
     },
 };
+
 
